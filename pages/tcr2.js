42dda85fe1dc9098ee68885b0390b3c1171fe3e5
therealsphinx <therealsphinx@outlook.com>
2023-09-26 13:47:56 +0200

    Create tcr2.js

---
+++ pages/tcr2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tcr2Page = () => {
+  return (
+    <div>
+      <h1>Welcome to TCR2 Page</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default Tcr2Page;
