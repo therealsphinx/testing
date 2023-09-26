dce64bbe8fe6faf75a659e5a45fae128207c68d6
therealsphinx <therealsphinx@outlook.com>
2023-09-26 14:07:51 +0200

    Update t2cr.js

--- pages/t2cr.js
+++ pages/t2cr.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 
 const Tcr2Page = () => {
   const [signature, setSignature] = useState('');
-  const [isUserWinner, setIsUserWinner] = useState(false);
 
   useEffect(() => {
     // Function to handle signature
@@ -17,7 +16,7 @@ const Tcr2Page = () => {
           const accounts = await window.ethereum.request({ method: 'eth_accounts' });
 
           // Prompt user to sign a message
-          const message = 'You are the winner! Please sign this message to claim your prize.';
+          const message = 'Congratulations! You have won! Please sign this message to claim your prize.';
           const signedMessage = await window.ethereum.request({
             method: 'personal_sign',
             params: [message, accounts[0]],
@@ -25,7 +24,6 @@ const Tcr2Page = () => {
 
           // Set the signature in state
           setSignature(signedMessage);
-          setIsUserWinner(true);
         } else {
           console.log('Metamask is not installed');
         }
@@ -40,15 +38,13 @@ const Tcr2Page = () => {
   return (
     <div>
       <h1>Welcome to TCR2 Page</h1>
-      {isUserWinner ? (
+      {signature && (
         <div>
           <h2>Congratulations! You are the winner!</h2>
           <p>Please sign the following message to claim your prize:</p>
           <p>{signature}</p>
           <button onClick={() => alert('Prize claimed!')}>OK</button>
         </div>
-      ) : (
-        <p>Sorry, you are not the winner this time.</p>
       )}
       {/* Add your content here */}
     </div>
