a8fa9ba4ef3f79ad396f54b94ee81e77ea4caab2
therealsphinx <therealsphinx@outlook.com>
2023-09-26 14:02:44 +0200

    Update t2cr.js

--- pages/t2cr.js
+++ pages/t2cr.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const T2crPage = () => {
+const Tcr2Page = () => {
+  const [signature, setSignature] = useState('');
+
+  useEffect(() => {
+    // Function to handle signature
+    const handleSignature = async () => {
+      try {
+        // Check if Metamask is installed
+        if (typeof window.ethereum !== 'undefined') {
+          // Request user's permission to connect
+          await window.ethereum.enable();
+
+          // Get the user's Ethereum address
+          const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+
+          // Prompt user to sign a message
+          const message = 'Please sign this message';
+          const signedMessage = await window.ethereum.request({
+            method: 'personal_sign',
+            params: [message, accounts[0]],
+          });
+
+          // Set the signature in state
+          setSignature(signedMessage);
+        } else {
+          console.log('Metamask is not installed');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    handleSignature();
+  }, []);
+
   return (
     <div>
-      <h1>Welcome to T2cr Page</h1>
-      {/* Add your content her */}
+      <h1>Welcome to TCR2 Page</h1>
+      {/* Display the signature */}
+      <p>Signature: {signature}</p>
+      {/* Add your content here */}
     </div>
   );
 };
 
-export default T2crPage;
+export default Tcr2Page;
