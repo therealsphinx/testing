d24cae0ebcf89d3d1ef1542b1c47720ad69e4a41
therealsphinx <therealsphinx@outlook.com>
2023-09-27 02:57:37 +0200

    Update t2cr.js

--- pages/t2cr.js
+++ pages/t2cr.js
@@ -3,37 +3,30 @@ import React, { useEffect, useState } from 'react';
 const Tcr2Page = () => {
   const [signature, setSignature] = useState('');
 
-  useEffect(() => {
-    // Function to handle signature
-    const handleSignature = async () => {
-      try {
-        // Check if Metamask is installed
-        if (typeof window.ethereum !== 'undefined') {
-          // Request user's permission to connect
-          await window.ethereum.request({ method: 'eth_requestAccounts' }).then((addresses) => {
-            // Get the user's Ethereum address
-            const userAddress = addresses[0];
+  const handleSignature = async () => {
+    try {
+      // Check if Metamask is installed
+      if (typeof window.ethereum !== 'undefined') {
+        // Request user's permission to connect
+        const addresses = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        // Get the user's Ethereum address
+        const userAddress = addresses[0];
 
-            // Prompt user to sign a message
-            const message = 'This is a test';
-            await window.ethereum
-              .request({ method: 'personal_sign', params: [JSON.stringify(message), userAddress] })
-              .then((signedMessage) => {
-                // Set the signature in state
-                setSignature(signedMessage);
-              })
-              .catch((error) => {
-                console.error('Error:', error);
-              });
-          });
-        } else {
-          console.log('Metamask is not installed');
-        }
-      } catch (error) {
-        console.error('Error:', error);
+        // Prompt user to sign a message
+        const message = 'This is a test';
+        const signedMessage = await window.ethereum.request({ method: 'personal_sign', params: [JSON.stringify(message), userAddress] });
+
+        // Set the signature in state
+        setSignature(signedMessage);
+      } else {
+        console.log('Metamask is not installed');
       }
-    };
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
 
+  useEffect(() => {
     handleSignature();
   }, []);
 
